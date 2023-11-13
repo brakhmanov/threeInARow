b071f9209f4aece2f72b8150b9919cbc594d1922
brakhmanov <dumorph@gmail.com>
2023-11-13 21:46:03 +0100

    First init

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a Scanner to get user input (name, position etc)
+        Scanner sc = new Scanner(System.in);
+
+        // Get player's names
+        System.out.println("Player 1, what is your name?");
+        String player1 = sc.nextLine();
+        System.out.println("Player 2, what is your name?");
+        String player2 = sc.nextLine();
+
+
+    }
+}
