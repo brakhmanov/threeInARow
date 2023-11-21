1ce5ed0e7b4c2b5706760d85cdd75574cfc1f4d6
brakhmanov <dumorph@gmail.com>
2023-11-21 02:42:12 +0100

    adding symbol variable to later use for player or computer user

--- src/Main.java
+++ src/Main.java
@@ -20,36 +20,9 @@ public class Main {
         int pos = scan.nextInt();
         System.out.println(pos);
 
-        // Change space character with X or O with switch loop
-        switch (pos) {
-            case 1:
-                gameBoard[0][0] = 'X';
-                break;
-            case 2:
-                gameBoard[0][0] = 'X';
-                break;
-            case 3:
-                gameBoard[0][0] = 'X';
-                break;
-            case 4:
-                gameBoard[0][0] = 'X';
-                break;
-            case 5:
-                gameBoard[0][0] = 'X';
-                break;
-            case 6:
-                gameBoard[0][0] = 'X';
-                break;
-            case 7:
-                gameBoard[0][0] = 'X';
-                break;
-            case 8:
-                gameBoard[0][0] = 'X';
-                break;
-            case 9:
-                gameBoard[0][0] = 'X';
-                break;
-        }
+
+        // Print game board with X that changed position
+        printGameBoard(gameBoard);
     }
 
     // Create the method to print game board
@@ -60,6 +33,50 @@ public class Main {
                 System.out.print(c);
             }
             System.out.println();
+        }
+    }
+
+    // Create own method for placing the X to reuse later
+    public static void placePiece(char[][] gameBoard, int pos, String user) {
+
+        char symbol = 'X';
+
+        if(user.equals("player")) {
+            symbol = 'X';
+        } else if (user.equals("cpu")) {
+            symbol = 'O';
+
+        }
+
+        // Change space character with X or O with switch loop
+        switch (pos) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
         }
     }
 }
