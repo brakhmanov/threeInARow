6d20aea6f0979f3b3a6d7a9a6b23275f48f56e50
brakhmanov <dumorph@gmail.com>
2023-11-21 03:49:59 +0100

    adding winning conditions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    // Player and cpu positions in array list
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
     public static void main(String[] args) {
 
         // Create 2D array for our board
@@ -15,25 +17,33 @@ public class Main {
         printGameBoard(gameBoard);
 
 
-        Scanner scan = new Scanner(System.in);
-        // Ask player to choose position to place X or O
-        System.out.println("Choose position (1-9): ");
-        int playerPos = scan.nextInt();
+        // Game loop
+        while (true) {
+            Scanner scan = new Scanner(System.in);
 
-        // Calling placePiece method for player to place X
-        placePiece(gameBoard, playerPos, "player");
+            // Ask player to choose position to place X or O
+            System.out.println("Choose position (1-9): ");
+            int playerPos = scan.nextInt();
 
-        // Same method but with Random for computer to place O
-        Random rand = new Random();
-        int cpuPos = rand.nextInt(9) + 1;
-        placePiece(gameBoard, cpuPos, "cpu");
+            // Calling placePiece method for player to place X
+            placePiece(gameBoard, playerPos, "player");
 
-        // Print game board with X that changed position
-        printGameBoard(gameBoard);
+            // Same method but with Random for computer to place O
+            Random rand = new Random();
+            int cpuPos = rand.nextInt(9) + 1;
+            placePiece(gameBoard, cpuPos, "cpu");
+
+            // Print game board with X that changed position
+            printGameBoard(gameBoard);
+
+            // Check the winner
+            String result = checkWinner();
+            System.out.println(result);
+        }
     }
 
     // Create the method to print game board
-    public static void printGameBoard(char[][]gameBoard) {
+    public static void printGameBoard(char[][] gameBoard) {
         // Print out the game board using 2 nested loops
         for (char[] row : gameBoard) {
             for (char c : row) {
@@ -50,11 +60,12 @@ public class Main {
         char symbol = ' ';
 
         // If playing against computer set the variable to O
-        if(user.equals("player")) {
+        if (user.equals("player")) {
             symbol = 'X';
+            playerPositions.add(pos);
         } else if (user.equals("cpu")) {
             symbol = 'O';
-
+            cpuPositions.add(pos);
         }
 
         // Change space character with X or O (symbol) with switch loop
@@ -89,5 +100,44 @@ public class Main {
             default:
                 break;
         }
+    }
+
+    // Check winner method
+    public static String checkWinner() {
+
+        // Row checker for every row with help of list
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+        // Row checker for columns
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        // Diagonals checker
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(7, 5, 3);
+
+        List<List> winConditions = new ArrayList<>();
+        winConditions.add(topRow);
+        winConditions.add(midRow);
+        winConditions.add(botRow);
+        winConditions.add(leftCol);
+        winConditions.add(midCol);
+        winConditions.add(rightCol);
+        winConditions.add(cross1);
+        winConditions.add(cross2);
+
+        for(List l : winConditions) {
+            if(playerPositions.containsAll(l)) {
+                return "Congratulations! YOU WON!";
+            } else if (cpuPositions.containsAll(l)) {
+                return "You lost. Game over :(";
+            } else if (playerPositions.size() + cpuPositions.size() == 9) {
+                return "No one wins. It is a tie. ";
+            }
+        }
+
+
+        return "";
     }
 }
